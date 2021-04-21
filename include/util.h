97e57c77e7b742fa172b2907603087ce0d687e68
Jake Massoth <jake@massoth.tech>
2021-04-21 20:05:56 +0200

    debug print

--- include/util.h
+++ include/util.h
@@ -1,8 +1,13 @@
-//
-// Created by jake on 4/21/21.
-//
-
 #ifndef MESSAGEFIRST_UTIL_H
 #define MESSAGEFIRST_UTIL_H
+
+#define DEBUG 1
+
+#ifdef DEBUG
+#define DEBUG_PRINT(fmt, ...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+    __FILE__, __LINE__, __func__, __VA_ARGS__)
+#else
+#define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#endif
 
 #endif //MESSAGEFIRST_UTIL_H

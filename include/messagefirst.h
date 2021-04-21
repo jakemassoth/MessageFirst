a69204aa9b98a1d78819cbb9460afe65d25715e5
Jake Massoth <jake@massoth.tech>
2021-04-21 20:06:24 +0200

    Error enum and handling

--- include/messagefirst.h
+++ include/messagefirst.h
@@ -1,14 +1,32 @@
 #ifndef MESSAGEFIRST_MESSAGEFIRST_H
 #define MESSAGEFIRST_MESSAGEFIRST_H
 #define MAX_DATA_LEN 128
+#include <sys/socket.h>
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
 
 struct __attribute__((packed)) mf_msg {
     char data[MAX_DATA_LEN];
-    long expect_resp;
+    int len;
 };
 
 struct __attribute__((packed)) mf_ctx {
     long timeout;
 };
+
+typedef enum mf_error_e {
+    MF_ERROR_OK = 0,
+    MF_ERROR_RECV_LEN = 1,
+    MF_ERROR_SEND = 2,
+    MF_ERROR_RECV_MSG = 3,
+
+    MF_ERROR_COUNT = 4
+} mf_error_t;
+
+const char *mf_error_str(mf_error_t e);
+
+void mf_error_print(mf_error_t e);
+
 
 #endif //MESSAGEFIRST_MESSAGEFIRST_H
--- messagefirst.c
+++ messagefirst.c
@@ -1,4 +1,21 @@
-//
-// Created by jake on 4/21/21.
-//
+#include "include/messagefirst.h"
 
+void mf_error_print(mf_error_t e) {
+    fprintf(stderr, "MessageFirst Error: %s\n", mf_error_str(e));
+}
+
+const char *mf_error_str(mf_error_t e) {
+    const char *MF_ERROR_STRS[] = {
+            "MF_ERROR_OK",
+            "MF_ERROR_RECV_LEN",
+            "MF_ERROR_SEND",
+            "MF_ERROR_RECV_MSG",
+    };
+
+    const char *str = NULL;
+
+    if (e >=  MF_ERROR_COUNT) return str;
+
+    str = MF_ERROR_STRS[e];
+    return str;
+}

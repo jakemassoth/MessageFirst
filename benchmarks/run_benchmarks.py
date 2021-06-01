b224ddc41cf7121885489bf47512fa680ee3458a
Jake Massoth <jake@massoth.tech>
2021-06-01 16:39:34 +0200

    whoops

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         netperf_stream_rr.run_benchmark_netperf.run(on_das=True, node1=sys.argv[1], node2=sys.argv[2])
         echo_benchmark.run_benchmark_echo.run(on_das=True, node1=sys.argv[1], node2=sys.argv[2])
     else:
-        # netperf_stream_rr.run_benchmark_netperf.run()
+        netperf_stream_rr.run_benchmark_netperf.run()
         echo_benchmark.run_benchmark_echo.run()
     plot_comparison_graphs.plot_comparison_graphs()
